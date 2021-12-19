2eba65e6b14c122461a575f1858d26850405971f
Tomasz Pietrzak <tomaszpietrzak14@gmail.com>
2021-12-19 20:55:00 +0100

    implemented get_visit_price method in visit/views.py

--- visits/serializers.py
+++ visits/serializers.py
@@ -4,7 +4,6 @@ from .models import Visit
 
 
 class VisitSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Visit
         fields = (
@@ -14,3 +13,24 @@ class VisitSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data['doctor'] = self.context['request'].user
         return super().create(validated_data)
+
+    '''
+    def update(self, instance, validated_data):
+        """Performs an update on a Visit."""
+
+        # Passwords should not be handled with `setattr`, unlike other fields.
+        # This is because Django provides a function that handles hashing and
+        # salting passwords, which is important for security. What that means
+        # here is that we need to remove the password field from the
+        # `validated_data` dictionary before iterating over it.
+
+        for (key, value) in validated_data.items():
+            # For the keys remaining in `validated_data`, we will set them on
+            # the current `Visit` instance one at a time.
+            setattr(instance, key, value)
+
+        # Finally, after everything has been updated, we must explicitly save
+        # the model. It's worth pointing out that `.set_password()` does not
+        # save the model.
+        instance.save()
+        '''
--- visits/views.py
+++ visits/views.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -16,3 +18,14 @@ class VisitViewSet(viewsets.ModelViewSet):
         obj.patient = request.user
         obj.save()
         return Response(status=201)
+
+    @action(detail=True, methods=['get'])
+    def get_visit_price(self, request, pk):
+        obj = self.get_object()
+        r = requests.get('http://api.nbp.pl/api/exchangerates/rates/a/eur/')
+        r_json = json.dumps(r.json())
+        rate_eur = json.loads(r_json)["rates"][0]["mid"]
+        price_eur = obj.price / rate_eur
+        obj.save()
+
+        return Response(["Cena PLN: " + "{:.2f}".format(obj.price), "Kurs EUR: " + "{:.2f}".format(rate_eur), "Cena EUR: " + "{:.2f}".format(price_eur)], status=201)
